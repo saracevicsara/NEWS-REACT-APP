06a5951ec902aa791e63b5ee2e9d8004e6d0e952
unknown <sarasaravevic@gmail.com>
2022-09-13 20:51:08 +0200

    update todo

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -4,18 +4,7 @@ import CreateToDo from "./CreateToDo";
 import ToDoList from "./ToDoList";
 
 export default function ToDo({ language }) {
-  const [tasks, setTasks] = useState([
-    {
-      name: "okupaj se",
-      description: "Trebal uopste opis",
-      date: "28.08.2022",
-    },
-    {
-      name: "Pij vodu",
-      description: "otvori flasu i piJ vodu",
-      date: "30.08.2022",
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   const addTask = (createTask) => {
     setTasks([...tasks, createTask]);
